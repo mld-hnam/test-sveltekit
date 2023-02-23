e7b669dfc76b1194f5f90bab1e68edc68c11a8d7
Tobias Messner <tobias.d.messner@gmail.com>
2023-02-23 15:17:03 +0100

    Revert "See if removing other things changes issue"
    This reverts commit 68a321c310381fae9fd195ef76b268e4cce34528.

---
+++ src/routes/no-prerender/+page.server.ts
@@ -0,0 +1,18 @@
+import type { PageServerLoad } from './$types';
+import type {Config} from "@sveltejs/adapter-vercel";
+import { nanoid } from "nanoid";
+
+export const config: Config = {
+    isr: {
+        expiration: 15
+    }
+}
+
+export const load: PageServerLoad = () => {
+
+    let id = nanoid();
+
+    return {
+        id
+    }
+}
---
+++ src/routes/no-prerender/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import type { PageData } from './$types';
+    export let data: PageData;
+</script>
+
+<h1>No Prerender</h1>
+<p>
+  ID: {data.id}
+</p>
+<p>
+  This should change every 15 seconds, but it always changes
+</p>
+<button on:click={() => window.location.reload()}>Refresh</button>
