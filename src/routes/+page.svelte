7a6bdfc8be1d676887aac5f492c44c52c139738e
Tobias Messner <tobias.d.messner@gmail.com>
2023-02-23 15:22:01 +0100

    Remove stuff from landing and remove split again

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,3 @@
-<script lang="ts">
-    import type { PageData } from './$types';
-    export let data: PageData;
-</script>
-
-<h1>prerender = 'auto'</h1>
-<p>
-  ID: {data.id}
-</p>
-
 <ul>
   <li><a href="/no-prerender">no prerender set (doesn't work)</a></li>
   <li><a href="/prerender-auto">prerender set to auto (doesn't work)</a></li>
